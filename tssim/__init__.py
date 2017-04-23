4688d48ceeb365174353ab710d03c39dda10a115
Franz Wöllert <franz.woellert@gmail.com>
2017-04-23 20:15:14 +0200

    Adjust module and class references to accessible from package top level.

--- tssim/__init__.py
+++ tssim/__init__.py
@@ -3,3 +3,9 @@
 __author__ = """Franz Woellert"""
 __email__ = 'franz.woellert@gmail.com'
 __version__ = '0.1.0'
+
+
+from tssim.core.series import TimeSeries
+from tssim.core.function import TimeFunction
+from tssim.core.track import TimeTrack
+from tssim.functions import random
